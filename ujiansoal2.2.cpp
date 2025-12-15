051b382613d54323a8de5fd4b9327c450a6e7813
reyhan-dot <reyhanalakmal@gmail.com>
2025-12-15 15:41:39 +0700

    Fix formatting and clean up output in triangle program

--- ujiansoal2.2.cpp
+++ ujiansoal2.2.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int main() {
 	int n;
-	cout << "Masukkan Jumlah baris:";//jumlah baris
+	cout << "Masukkan Jumlah baris:";
 	cin >> n;
 	for (int i = n - 1; i >= 0; --i) {//untuk segitiga terbalik
 
-		for (int k = 0; k < n - 1 - i; k++) { //untuk menata segitiga
+		for (int k = 0; k < n - 1 - i; k++) { 
 			cout << " ";
 		}
 
-		int val = 1;   //nilai pertama
+		int val = 1;   
 		for (int j = 0; j <= i; j++) {
 
 			
-			val = val * (i - j) / (j + 1);  //rumus segitiga
+			val = val * (i - j) / (j + 1);  
 			cout << "* ";	
 		}
 		cout << endl;
